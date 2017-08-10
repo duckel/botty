c48a2640041d17e3de024e01e1f2e2c456d09d49
duckel <30893109+duckel@users.noreply.github.com>
2017-08-10 10:37:27 +0200

    Create secrets.js

---
+++ config/secrets.js
@@ -0,0 +1,15 @@
+{
+  "discord": {
+    "token": "roughly 60 character token",
+    "BOTID": "MzQ0Nzg1MTE3MDQwMDgyOTQ1.DG2qxQ.Ao0SpmK17puM7YsUcZPHmZj-vv4",
+	"ROLE_IDS": {
+	  "@legendary": "18 character numeric id"
+	}
+  },
+  "mysql": {
+    "host": "localhost",
+    "user": "admin",
+    "password": "password",
+    "database": "discordDB"
+  }
+}
